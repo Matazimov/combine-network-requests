b134c3a58b3a8f71263a4c03a8566f6519a823b2
matazimov <ilkhomzhonmatazimov@gmail.com>
2022-04-11 23:04:36 +0500

    Initial Commit

---
+++ combine-network-requests/combine_network_requestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_network_requestsApp.swift
+//  combine-network-requests
+//
+//  Created by Ilkhomzhon on 11.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct combine_network_requestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
